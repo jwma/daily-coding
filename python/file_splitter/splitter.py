02b473b32bff699f56c558133e9701e379863a82
jwma <mjw@majiawei.com>
2017-09-03 17:38:17 +0800

    python文件切割的模块，使用类封装的方式重写了基于纯函数的写法

--- python/file_splitter/splitter.py
+++ python/file_splitter/splitter.py
@@ -1,49 +1,58 @@
-def write_new_file(file_idx=0, content=''):
-    new_file = open('./result/{0}.txt'.format(file_idx), 'w')
-    new_file.writelines(content)
-    new_file.close()
+import os
 
 
-def split_file(filename='', count=500, clean_line_handler=None, mk_content_handler=None):
-    file_idx = 0
-    counter = 1
-    lines = []
+class Splitter(object):
+    def __write_new_file(self, file_idx=0, content='', file_ext='', **kwargs):
+        prefix = kwargs.get('prefix', '')
+        try:
+            with open('./result/{0}{1}{2}'.format(prefix, file_idx, file_ext), 'w') as new_file:
+                new_file.writelines(content)
+            return True
+        except IOError as e:
+            print(e)
+            return False
 
-    has_clean_line_handler = callable(clean_line_handler)
-    has_mk_content_handler = callable(mk_content_handler)
+    def split_file(self, filename='', count=500, clean_line_handler=None, mk_content_handler=None, **kwargs):
+        counter = 1
+        lines = []
+        file_idx = 0
+        file_ext = os.path.splitext(filename)[1]
 
-    try:
-        with open(filename, 'r') as target_file:
-            for line in target_file:
-                if has_clean_line_handler:
-                    lines.append(clean_line_handler(line))
-                else:
-                    lines.append(line)
+        has_clean_line_handler = callable(clean_line_handler)
+        has_mk_content_handler = callable(mk_content_handler)
 
-                if counter > count:
-                    if has_mk_content_handler:
-                        write_new_file(file_idx, mk_content_handler(lines))
+        try:
+            with open(filename, 'r') as target_file:
+                for line in target_file:
+                    if has_clean_line_handler:
+                        lines.append(clean_line_handler(line))
                     else:
-                        write_new_file(file_idx, ''.join(lines))
+                        lines.append(line)
 
-                    lines = []
-                    counter = 1
-                    file_idx += 1
+                    if counter >= count:
+                        if has_mk_content_handler:
+                            self.__write_new_file(file_idx, mk_content_handler(lines), file_ext, **kwargs)
+                        else:
+                            self.__write_new_file(file_idx, ''.join(lines), file_ext, **kwargs)
 
-                    continue
+                        lines = []
+                        counter = 1
+                        file_idx += 1
 
-                counter += 1
+                        continue
 
-            if len(lines) > 0:
-                if has_mk_content_handler:
-                    write_new_file(file_idx, mk_content_handler(lines))
-                else:
-                    write_new_file(file_idx, ''.join(lines))
+                    counter += 1
+
+                if len(lines) > 0:
+                    if has_mk_content_handler:
+                        self.__write_new_file(file_idx, mk_content_handler(lines), file_ext, **kwargs)
+                    else:
+                        self.__write_new_file(file_idx, ''.join(lines), file_ext, **kwargs)
 
             return True
-    except IOError as e:
-        print(e)
-        return False
+        except IOError as e:
+            print(e)
+            return False
 
 
 if __name__ == '__main__':
@@ -55,4 +64,19 @@ if __name__ == '__main__':
         return ','.join(lines)
 
 
-    print(split_file('./test_data.txt', 3, my_clean_line_handler, my_mk_content_handler))
+    splitter = Splitter()
+
+    """
+    分割test_data.txt文件，
+    每个文件包含2行，
+    生成的分块文件前缀为t1
+    """
+    print(splitter.split_file('./test_data.txt', 2, prefix='t1_'))
+
+    """
+    分割test_data.txt文件，每个文件包含4行，
+    每行内容经过my_clean_line_handler处理，
+    每次写入分块文件前内容经过my_mk_content_handler处理
+    生成的分块文件前缀为t2
+    """
+    print(splitter.split_file('./test_data.txt', 4, my_clean_line_handler, my_mk_content_handler, prefix='t2_'))
